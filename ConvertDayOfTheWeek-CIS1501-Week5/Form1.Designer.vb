7f9fbaf78de814b5e8d4e93de46dc8d41e8ee357
ShabbyPenguin <ShabbyPenguin@gmail.com>
2017-06-04 20:53:05 -0400

    Finished the class assignment

--- ConvertDayOfTheWeek-CIS1501-Week5/Form1.Designer.vb
+++ ConvertDayOfTheWeek-CIS1501-Week5/Form1.Designer.vb
@@ -22,9 +22,90 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.userInputBox = New System.Windows.Forms.TextBox()
+        Me.dayOfTheWeekBox = New System.Windows.Forms.TextBox()
+        Me.calculateBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'clearBtn
+        '
+        Me.clearBtn.Location = New System.Drawing.Point(12, 154)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(95, 39)
+        Me.clearBtn.TabIndex = 0
+        Me.clearBtn.Text = "Clear"
+        Me.clearBtn.UseVisualStyleBackColor = True
+        '
+        'exitBtn
+        '
+        Me.exitBtn.Location = New System.Drawing.Point(113, 154)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(95, 39)
+        Me.exitBtn.TabIndex = 1
+        Me.exitBtn.Text = "E&xit"
+        Me.exitBtn.UseVisualStyleBackColor = True
+        '
+        'userInputBox
+        '
+        Me.userInputBox.Location = New System.Drawing.Point(14, 40)
+        Me.userInputBox.Name = "userInputBox"
+        Me.userInputBox.Size = New System.Drawing.Size(193, 20)
+        Me.userInputBox.TabIndex = 3
+        Me.userInputBox.Text = "Enter a number"
+        '
+        'dayOfTheWeekBox
+        '
+        Me.dayOfTheWeekBox.Location = New System.Drawing.Point(14, 88)
+        Me.dayOfTheWeekBox.Name = "dayOfTheWeekBox"
+        Me.dayOfTheWeekBox.ReadOnly = True
+        Me.dayOfTheWeekBox.Size = New System.Drawing.Size(193, 20)
+        Me.dayOfTheWeekBox.TabIndex = 5
+        '
+        'calculateBtn
+        '
+        Me.calculateBtn.Location = New System.Drawing.Point(12, 118)
+        Me.calculateBtn.Name = "calculateBtn"
+        Me.calculateBtn.Size = New System.Drawing.Size(195, 30)
+        Me.calculateBtn.TabIndex = 6
+        Me.calculateBtn.Text = "Calculate"
+        Me.calculateBtn.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 72)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(135, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Matching Day of the Week"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(219, 201)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.calculateBtn)
+        Me.Controls.Add(Me.dayOfTheWeekBox)
+        Me.Controls.Add(Me.userInputBox)
+        Me.Controls.Add(Me.exitBtn)
+        Me.Controls.Add(Me.clearBtn)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Day of the week calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents exitBtn As Button
+    Friend WithEvents userInputBox As TextBox
+    Friend WithEvents dayOfTheWeekBox As TextBox
+    Friend WithEvents calculateBtn As Button
+    Friend WithEvents Label1 As Label
 End Class
--- ConvertDayOfTheWeek-CIS1501-Week5/Form1.vb
+++ ConvertDayOfTheWeek-CIS1501-Week5/Form1.vb
@@ -1,3 +1,35 @@
 ﻿Public Class Form1
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        Application.Exit()
+    End Sub
 
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        'This clears both boxes when you click the clear button
+        userInputBox.Text = ""
+        dayOfTheWeekBox.Text = ""
+    End Sub
+
+    Private Sub calculateBtn_Click(sender As Object, e As EventArgs) Handles calculateBtn.Click
+        'We check if user has entered a number and if not alert them
+        If Not IsNumeric(userInputBox.Text) Then
+            MessageBox.Show("Please enter in a number")
+        Else
+            'We then need to check if its between 1 to 7
+            If (userInputBox.Text <= 7 AndAlso userInputBox.Text > 0) Then
+                'This takes the number and provies the day of the week name based on system settings
+                dayOfTheWeekBox.Text = WeekdayName(userInputBox.Text)
+            Else
+                'We throw this error message if the user input a number that wouldn't work
+                MessageBox.Show("Please enter in a number lower than 8 but higher than 0")
+            End If
+        End If
+        Exit Sub
+    End Sub
+
+    Private Sub userInputBox_Click(sender As Object, e As EventArgs) Handles userInputBox.Click
+        'Clearing out the userinput box for them as they click onto it.
+        If userInputBox.Text = "Enter a number" Then
+            userInputBox.Clear()
+        End If
+    End Sub
 End Class
