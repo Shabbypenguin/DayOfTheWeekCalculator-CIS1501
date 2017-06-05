86e983ee0122fd1b46b713db22eec3459fd199c9
ShabbyPenguin <ShabbyPenguin@gmail.com>
2017-06-04 21:24:13 -0400

    Minor updates

--- ConvertDayOfTheWeek-CIS1501-Week5/Form1.Designer.vb
+++ ConvertDayOfTheWeek-CIS1501-Week5/Form1.Designer.vb
@@ -32,61 +32,67 @@ Partial Class Form1
         '
         'clearBtn
         '
-        Me.clearBtn.Location = New System.Drawing.Point(12, 154)
+        Me.clearBtn.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.clearBtn.Location = New System.Drawing.Point(12, 133)
         Me.clearBtn.Name = "clearBtn"
         Me.clearBtn.Size = New System.Drawing.Size(95, 39)
-        Me.clearBtn.TabIndex = 0
-        Me.clearBtn.Text = "Clear"
+        Me.clearBtn.TabIndex = 2
+        Me.clearBtn.Text = "Cle&ar"
         Me.clearBtn.UseVisualStyleBackColor = True
         '
         'exitBtn
         '
-        Me.exitBtn.Location = New System.Drawing.Point(113, 154)
+        Me.exitBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.exitBtn.Location = New System.Drawing.Point(113, 133)
         Me.exitBtn.Name = "exitBtn"
         Me.exitBtn.Size = New System.Drawing.Size(95, 39)
-        Me.exitBtn.TabIndex = 1
+        Me.exitBtn.TabIndex = 3
         Me.exitBtn.Text = "E&xit"
         Me.exitBtn.UseVisualStyleBackColor = True
         '
         'userInputBox
         '
-        Me.userInputBox.Location = New System.Drawing.Point(14, 40)
+        Me.userInputBox.Location = New System.Drawing.Point(14, 19)
         Me.userInputBox.Name = "userInputBox"
         Me.userInputBox.Size = New System.Drawing.Size(193, 20)
-        Me.userInputBox.TabIndex = 3
+        Me.userInputBox.TabIndex = 0
         Me.userInputBox.Text = "Enter a number"
         '
         'dayOfTheWeekBox
         '
-        Me.dayOfTheWeekBox.Location = New System.Drawing.Point(14, 88)
+        Me.dayOfTheWeekBox.Location = New System.Drawing.Point(14, 67)
         Me.dayOfTheWeekBox.Name = "dayOfTheWeekBox"
         Me.dayOfTheWeekBox.ReadOnly = True
         Me.dayOfTheWeekBox.Size = New System.Drawing.Size(193, 20)
         Me.dayOfTheWeekBox.TabIndex = 5
+        Me.dayOfTheWeekBox.TabStop = False
         '
         'calculateBtn
         '
-        Me.calculateBtn.Location = New System.Drawing.Point(12, 118)
+        Me.calculateBtn.Location = New System.Drawing.Point(12, 97)
         Me.calculateBtn.Name = "calculateBtn"
         Me.calculateBtn.Size = New System.Drawing.Size(195, 30)
-        Me.calculateBtn.TabIndex = 6
-        Me.calculateBtn.Text = "Calculate"
+        Me.calculateBtn.TabIndex = 1
+        Me.calculateBtn.Text = "&Calculate"
         Me.calculateBtn.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(45, 72)
+        Me.Label1.Location = New System.Drawing.Point(45, 51)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(135, 13)
-        Me.Label1.TabIndex = 7
+        Me.Label1.TabIndex = 4
         Me.Label1.Text = "Matching Day of the Week"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'Form1
         '
+        Me.AcceptButton = Me.calculateBtn
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.CancelButton = Me.exitBtn
         Me.ClientSize = New System.Drawing.Size(219, 201)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.calculateBtn)
@@ -94,9 +100,13 @@ Partial Class Form1
         Me.Controls.Add(Me.userInputBox)
         Me.Controls.Add(Me.exitBtn)
         Me.Controls.Add(Me.clearBtn)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Day of the week calculator"
+        Me.Text = "Day of the Week Calculator"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
